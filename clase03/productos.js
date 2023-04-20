68f605e1ee92e85dfb83ec0f3d2297f0122656ae
gabyspina <gabyspina@gmail.com>
2023-04-19 21:58:49 -0300

    aplicando correcciones en el repo

--- clase03/productos.js
+++ clase03/productos.js
@@ -1,16 +1,27 @@
-class productManager {
+class ProductManager {
 	#id = 0;
 
-	construtor() {
-		this.productos = [];
+	constructor() {
+		this.products = [];
 	}
 
 	getProducts() {
-		return this.productos;
+		return this.products;
 	}
 
 	addProduct(title, description, price, thumbnail, code, stock) {
+		if (!title || !description || !price || !thumbnail || !code || !stock) {
+			console.error('Todos los campos son obligatorios.');
+			return;
+		}
+
+		if (this.products.some((product) => product.code === code)) {
+			console.error('Ya existe un producto con el mismo código.');
+			return;
+		}
+
 		const product = {
+			id: this.#getId(),
 			title,
 			description,
 			price,
@@ -19,9 +30,7 @@ class productManager {
 			stock,
 		};
 
-		product.id = this.#getId();
-
-		this.productos.push(product);
+		this.products.push(product);
 	}
 
 	#getId() {
@@ -30,12 +39,40 @@ class productManager {
 		return oldId;
 	}
 
-	#getProductsById() {
-		this.#id += 1;
-		const oldId = this.#id;
-		return oldId;
+	getProductById(id) {
+		const product = this.products.find((p) => p.id === id);
+
+		if (!product) {
+			console.error('Producto no encontrado.');
+			return null;
+		}
+
+		return product;
 	}
 }
 
-//productManager.addProduct('Producto 1', 'Descripcion 1', 100, 'https://www.google.com', 1, 10);
-productManager.getProducts();
+const manager = new ProductManager();
+
+manager.addProduct(
+	'Producto 1',
+	'Descripción 1',
+	100,
+	'https://www.google.com',
+	1,
+	10
+);
+
+manager.addProduct(
+	'Producto 2',
+	'Descripción 2',
+	200,
+	'https://www.google.com',
+	2,
+	20
+);
+
+console.log(manager.getProducts());
+
+console.log(manager.getProductById(0));
+
+console.log(manager.getProductById(1));
