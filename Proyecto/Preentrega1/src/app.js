c60b7209ded9aa0fae9496b05eda990e10787389
gabyspina <gabyspina@gmail.com>
2023-05-16 19:13:11 -0300

    modifico ultimos detalles para la entrega

--- Proyecto/Preentrega1/src/app.js
+++ Proyecto/Preentrega1/src/app.js
@@ -1,56 +1,14 @@
 import express from 'express';
-import { cartRouter } from './routers/cart.router.js';
-import { productsRouter } from './routers/products.router.js';
-import ProductManager from './productManager.js';
-
-const productManager = new ProductManager();
+import { productsRouter } from './routes/products.router.js';
+ import { cartsRouter } from './routes/carts.router.js';
 
 const app = express();
 
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Server
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
-const PORT = 8080;
-
-const server = app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
-
-//Routes
-
-app.use('/api/productos', productsRouter);
-app.use('/api/carrito', cartRouter);
-
- // Rutas
-
-app.get('/api/products', async (req, res) => {
-	res.send(await productManager.getProducts());
-
-});
-
-app.post('/api/products', async (req, res) => {
-	const product = req.body;
-	res.send(await productManager.addProduct(product));
-});
-
-// app.get('/api/products/:id', async (req, res) => {
-// 	try {
-// 		const id = req.params.id;
-// 		const products = await productManager.getProductById(id);
-// 		res.json(products);
-// 	} catch (err) {
-// 		console.log(err);
-// 	}
-// });
-
-// app.post('/api/products', async (req, res) => {
-// 	try {
-// 		const product = req.body;
-// 		res.json(product);
-// 	} catch (err) {
-// 		console.log(err);
-// 	}
-// });
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
--- Proyecto/Preentrega1/src/productManager.js
+++ Proyecto/Preentrega1/src/productManager.js
@@ -1,66 +1,87 @@
 import fs from 'fs';
-
 export default class ProductManager {
 	#id = 0;
 	constructor() {
-		this.path = './src/productos.json';
+		this.path = './src/products.json';
 		this.products = [];
-		//		fs.promises.writeFile(this.path, JSON.stringify([]) + '\n');
 	}
 
-	#getId() {
-		const oldId = this.#id;
-		this.#id += 1;
-		return oldId;
-	}
-	async addProduct(title, description, price, thumbnail, code, stock) {
-		if (!title || !description || !price || !thumbnail || !code || !stock) {
-			console.error('Todos los campos son obligatorios.');
-			return;
-		}
-		const product = {
-			id: this.#getId(),
-			title,
-			description,
-			price,
-			thumbnail,
-			code,
-			stock,
-		};
+	async addProduct(product) {
+		if ( 
+			!product.title ||
+			!product.description ||
+			!product.price ||
+			!product.thumbnail ||
+			!product.code ||
+			!product.stock ||
+			!product.category
+		)
+			return {
+				status: 'error',
+				message: 'Todos los campos son obligatorios.',
+			};
 		try {
-			const actualProducts = await this.getProducts();
-			actualProducts.push(product);
-			await fs.promises.writeFile(
-				this.path,
-				JSON.stringify([...actualProducts])
-			);
-		} catch (err) {
-			console.log(err);
+			if (fs.existsSync(this.path)) {
+				const data = await fs.promises.readFile(this.path, 'utf-8');
+				this.products = JSON.parse(data);
+				let id = this.products[this.products.length - 1].id + 1;
+				product.id = id;
+				this.products.push(product);
+				await fs.promises.writeFile(
+					this.path,
+					JSON.stringify(this.products, null, '\t')
+				);
+				return {
+					status: 'success',
+					message: 'Producto agregado',
+				};
+			} else {
+				product.id = 1;
+				this.products.push(product);
+				await fs.promises.writeFile(
+					this.path,
+					JSON.stringify(this.products, null, '\t')
+				);
+				return {
+					console: 'success',
+					message: 'Producto agregado',
+				};
+			}
+		} catch (error) {
+			console.log(error);
+			return {
+				status: 'error',
+				message: 'No se pudo agregar el producto',
+			};
 		}
 	}
 
 	async getProducts() {
 		try {
-			const actualProducts = await fs.promises.readFile(this.path, 'utf-8');
-			return JSON.parse(actualProducts);
+			const data = await fs.promises.readFile(this.path, 'utf-8');
+			return JSON.parse(data);
 		} catch (error) {
 			console.log(error);
+			return;
 		}
 	}
 
 	async getProductById(id) {
 		const products = await this.getProducts();
-		const product = products.find((product) => product.id === id);
+		const product = products.find((product) => product.id === parseInt(id));
 		if (!product) {
-			console.error('No se encontro el producto solicitado.');
-			return 'No se encontro el producto solicitado.';
+			console.error('Producto no encontrado.');
+			return { status: 'error', message: 'Producto no encontrado.' };
 		}
 		return product;
+	}
+	catch(err) {
+		console.log(err);
 	}
 
 	async updateProduct(id, archivoActualizado) {
 		const products = await this.getProducts();
-		const indice = products.findIndex((product) => product.id === id);
+		const indice = products.findIndex((product) => product.id === parseInt(id));
 		if (indice === -1) {
 			console.error('Producto no encontrado.');
 			return;
@@ -87,7 +108,7 @@ export default class ProductManager {
 		try {
 			const actualProducts = await this.getProducts();
 			const updateProduct = actualProducts.filter(
-				(product) => product.id !== id
+				(product) => product.id !== parseInt(id)
 			);
 			await fs.promises.writeFile(
 				this.path,
