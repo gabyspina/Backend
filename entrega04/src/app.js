4d17ad0739f43ebec66f13133d363ba1f2226a87
gabyspina <gabyspina@gmail.com>
2023-05-17 23:09:44 -0300

    agrego webSocket

--- entrega04/src/app.js
+++ entrega04/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import productRouter from './routes/products.router.js';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -13,6 +14,12 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRouter);
 
-app.listen(8080, () => {
+const httpServer = app.listen(8080, () => {
 	console.log('Server is running on port 8080');
+});
+
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+	console.log('New connection');
 });
