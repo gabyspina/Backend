d816eae986bc8daf349e0ff83af8305bd70ee64a
gabyspina <gabyspina@gmail.com>
2023-06-25 15:37:12 -0300

    finalizando entrega

--- segundaPreEntrega/src/app.js
+++ segundaPreEntrega/src/app.js
@@ -25,7 +25,7 @@ app.use('/api/carts', cartRouter);
 // app.use('/', realtimeProducts )
 // app.use('/api/chat', chatRouter);
 
-mongoose.connect('mongodb+srv://canuzamdq:boca2011@cluster0.ynmu0on.mongodb.net/ecommerce?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://gabyspina:gsp246813579@coderclaster.gnpohje.mongodb.net/ecommerce?retryWrites=true&w=majority');
 
 app.listen(8080, () => {
     console.log('Escuchando puerto 8080...');
--- segundaPreEntrega/src/services/product.service.js
+++ segundaPreEntrega/src/services/product.service.js
@@ -1,87 +1,86 @@
-import {productModel} from "../models/products.model.js";
-
+import { productModel } from '../models/products.model.js';
 
 class ProductService {
-    constructor() {
-        this.model = productModel;
-    }
+	constructor() {
+		this.model = productModel;
+	}
 
-    async getAllProductsSinPaginate() {
-        return await this.productModel.find().lean();
-    }
+	async getAllProductsSinPaginate() {
+		return await this.productModel.find().lean();
+	}
 
+	async getAllProducts(
+		limit = 10,
+		page = 1,
+		status = undefined,
+		category = undefined,
+		sort = undefined
+	) {
+		const filter = {};
+		const orderSort = {};
 
-    async getAllProducts(limit = 10, page = 1, status = undefined, category = undefined, sort = undefined) {
-        const filter = {};
-        const orderSort = {};
+		if (status === 'true') {
+			filter.status = true;
+		} else if (status === 'false') {
+			filter.status = false;
+		}
 
-        if (status === 'true') {
-            filter.status = true;
-        } else if (status === 'false') {
-            filter.status = false;
-        }
+		if (category === 'alimento ') {
+			filter.category = 'alimento ';
+		}
+		if (category === 'accesorios') {
+			filter.category = 'accesorios';
+		}
+		if (category === 'medicacion') {
+			filter.category = 'medicacion';
+		}
+		const queryOptions = {
+			lean: true,
+			page,
+			limit,
+			category,
+			sort: undefined,
+		};
 
-        if (category === 'monitores') {
-            filter.category = 'monitores';
-        }
-        if (category === 'accesorios') {
-            filter.category = 'accesorios';
-        }
-        if (category === 'tablets') {
-            filter.category = 'tablets';
-        }
-        if (category === 'notebooks') {
-            filter.category = 'notebooks';
-        }
+		// Verifica el valor de sort antes de ejecutar return await this.model.paginate(filter, queryOptions)
+		if (sort === 'desc') {
+			queryOptions.sort = { price: -1 };
+		} else if (sort === 'asc') {
+			queryOptions.sort = { price: 1 };
+		}
 
-        const queryOptions = {
-            lean: true,
-            page,
-            limit,
-            category,
-            sort: undefined
-        }
+		return await this.model.paginate(filter, queryOptions);
+	}
 
-        // Verifica el valor de sort antes de ejecutar return await this.model.paginate(filter, queryOptions)
-        if (sort === 'desc') {
-            queryOptions.sort = { price: -1 };
-        } else if (sort === 'asc') {
-            queryOptions.sort = { price: 1 };
-        }
+	async getProductById(productId) {
+		try {
+			const product = await this.model.findById(productId);
+			if (!product) {
+				throw new Error('Producto no encontrado');
+			}
+			return product;
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+	}
 
-        return await this.model.paginate(filter, queryOptions)
+	async addProduct(product) {
+		return this.model.create(product);
+	}
 
+	async updateProduct(productId, product) {
+		return await this.model.updateOne(
+			{
+				_id: productId,
+			},
+			product
+		);
+	}
 
-    };
-
-
-    async getProductById(productId) {
-        try {
-            const product = await this.model.findById(productId);
-            if (! product) {
-                throw new Error('Producto no encontrado');
-            }
-            return product;
-        } catch (err) {
-            console.error(err);
-            throw err;
-        }
-    }
-
-    async addProduct(product) {
-        return this.model.create(product);
-    }
-
-    async updateProduct(productId, product) {
-        return await this.model.updateOne({
-            _id: productId
-        }, product);
-    }
-
-    async deletePruduct(productId) {
-        return await this.model.deleteOne({_id: productId})
-    }
-
+	async deletePruduct(productId) {
+		return await this.model.deleteOne({ _id: productId });
+	}
 }
 
-export const productService = new ProductService;
+export const productService = new ProductService();
